324511aaffe1ba8daf6c2ebea55c095e3572765d
ikiriappcsi <ikiricsi@gmail.com>
2025-06-04 07:27:15 +0200

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product');
+const Product = require('./models/Product');
 
 // GET /api/v1/products - Összes termék lekérése
 router.get('/', async (req, res) => {
